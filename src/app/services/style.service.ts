6a16cbbbced1e8f206dfd2c44c2308e81bc9cd55
CarlosFTG <cftejedag@gmail.com>
2021-01-15 10:22:09 +0100

    uncomment method

--- src/app/services/style.service.ts
+++ src/app/services/style.service.ts
@@ -345,64 +345,64 @@ export class StyleService {
       bikeStationFeature.setStyle(markerStyle);
     }
 
-    // applyStyleToFoundAdressMarker(foundAddressFeature){
-    //   let markerStyle = [];
-    //   markerStyle.push(
-    //     new Style({
-    //     image: new IconStyle({
-    //       anchor: [0.5, 1],
-    //       anchorXUnits: 'fraction',
-    //       anchorYUnits: 'fraction',
-    //       opacity: 1,
-    //       src: this.assets_base + 'vehicle_pin.png',
-    //       snapToPixel: false
-    //     }),
-    //     //Text Style
-    //     text: new Text({
-    //       textAlign: 'center',
-    //       font: '9px',
-    //       textBaseline: 'top',
-    //       //text: vehicleInfo.name,
-    //       scale: 1.5,
-    //       offsetX: 0,
-    //       offsetY: 4,
+     applyStyleToFoundAdressMarker(foundAddressFeature){
+       let markerStyle = [];
+       markerStyle.push(
+         new Style({
+         image: new IconStyle({
+           anchor: [0.5, 1],
+           anchorXUnits: 'fraction',
+           anchorYUnits: 'fraction',
+           opacity: 1,
+           src: this.assets_base + 'vehicle_pin.png',
+           snapToPixel: false
+         }),
+         //Text Style
+         text: new Text({
+           textAlign: 'center',
+           font: '9px',
+           textBaseline: 'top',
+           //text: vehicleInfo.name,
+           scale: 1.5,
+           offsetX: 0,
+           offsetY: 4,
     //       // fill: new Fill({
     //       //   color: textoVehiculo_color
     //       // })
     //       // ,
-    //       stroke: new Stroke({
-    //         width: 0
-    //       })
-    //     })
-    //   })
-    //   );
+           stroke: new Stroke({
+             width: 0
+           })
+         })
+       })
+       );
   
-    //     markerStyle.push(
-    //       new Style({
-    //         image: new IconStyle(({
-    //           anchor: [21, 70],
-    //           anchorXUnits: 'pixels',
-    //           anchorYUnits: 'pixels',
-    //           opacity: 1,
-    //           src: this.assets_base + 'vehicle_color_7.png',
-    //           snapToPixel: false
-    //         })),
+         markerStyle.push(
+           new Style({
+             image: new IconStyle(({
+               anchor: [21, 70],
+               anchorXUnits: 'pixels',
+               anchorYUnits: 'pixels',
+               opacity: 1,
+               src: this.assets_base + 'vehicle_color_7.png',
+               snapToPixel: false
+             })),
     //         //Vehicle Label
-    //         text: new Text({
-    //           // text:
-    //           textAlign: 'center',
-    //           font: '11px Arial',
-    //           textBaseline: 'top',
-    //           fill: new Fill({ color: 'lightCyan' }),
-    //           stroke: new Stroke({ color: 'MediumBlue', width: 1 }),
-    //           offsetX: 0,
-    //           offsetY: 4,
-    //         })
-    //       }),
-    //     )
+             text: new Text({
+               // text:
+               textAlign: 'center',
+               font: '11px Arial',
+               textBaseline: 'top',
+               fill: new Fill({ color: 'lightCyan' }),
+               stroke: new Stroke({ color: 'MediumBlue', width: 1 }),
+               offsetX: 0,
+               offsetY: 4,
+             })
+           }),
+         )
       
-    //   foundAddressFeature.setStyle(markerStyle);
-    // }
+       foundAddressFeature.setStyle(markerStyle);
+     }
 
     applyStyleToDistricts(feature){
       let plots_styles = [];
