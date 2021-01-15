7897edf8bc726edc8b6980fdbcb58d98ef9a1654
CarlosFTG <cftejedag@gmail.com>
2021-01-15 09:04:19 +0100

    selected bs style

--- src/app/services/style.service.ts
+++ src/app/services/style.service.ts
@@ -246,7 +246,7 @@ export class StyleService {
                 anchorXUnits: 'pixels',
                 anchorYUnits: 'pixels',
                 opacity: 1,
-                src: this.assets_base + 'less.png',
+                src: this.assets_base + 'vehicle_color_2.png',
                 snapToPixel: false
               })),
               //Vehicle Label
@@ -345,64 +345,64 @@ export class StyleService {
       bikeStationFeature.setStyle(markerStyle);
     }
 
-    applyStyleToFoundAdressMarker(foundAddressFeature){
-      let markerStyle = [];
-      markerStyle.push(
-        new Style({
-        image: new IconStyle({
-          anchor: [0.5, 1],
-          anchorXUnits: 'fraction',
-          anchorYUnits: 'fraction',
-          opacity: 1,
-          src: this.assets_base + 'vehicle_pin.png',
-          snapToPixel: false
-        }),
-        //Text Style
-        text: new Text({
-          textAlign: 'center',
-          font: '9px',
-          textBaseline: 'top',
-          //text: vehicleInfo.name,
-          scale: 1.5,
-          offsetX: 0,
-          offsetY: 4,
-          // fill: new Fill({
-          //   color: textoVehiculo_color
-          // })
-          // ,
-          stroke: new Stroke({
-            width: 0
-          })
-        })
-      })
-      );
+    // applyStyleToFoundAdressMarker(foundAddressFeature){
+    //   let markerStyle = [];
+    //   markerStyle.push(
+    //     new Style({
+    //     image: new IconStyle({
+    //       anchor: [0.5, 1],
+    //       anchorXUnits: 'fraction',
+    //       anchorYUnits: 'fraction',
+    //       opacity: 1,
+    //       src: this.assets_base + 'vehicle_pin.png',
+    //       snapToPixel: false
+    //     }),
+    //     //Text Style
+    //     text: new Text({
+    //       textAlign: 'center',
+    //       font: '9px',
+    //       textBaseline: 'top',
+    //       //text: vehicleInfo.name,
+    //       scale: 1.5,
+    //       offsetX: 0,
+    //       offsetY: 4,
+    //       // fill: new Fill({
+    //       //   color: textoVehiculo_color
+    //       // })
+    //       // ,
+    //       stroke: new Stroke({
+    //         width: 0
+    //       })
+    //     })
+    //   })
+    //   );
   
-        markerStyle.push(
-          new Style({
-            image: new IconStyle(({
-              anchor: [21, 70],
-              anchorXUnits: 'pixels',
-              anchorYUnits: 'pixels',
-              opacity: 1,
-              src: this.assets_base + 'vehicle_color_7.png',
-              snapToPixel: false
-            })),
-            //Vehicle Label
-            text: new Text({
-              // text:
-              textAlign: 'center',
-              font: '11px Arial',
-              textBaseline: 'top',
-              fill: new Fill({ color: 'lightCyan' }),
-              stroke: new Stroke({ color: 'MediumBlue', width: 1 }),
-              offsetX: 0,
-              offsetY: 4,
-            })
-          }),
-        )
+    //     markerStyle.push(
+    //       new Style({
+    //         image: new IconStyle(({
+    //           anchor: [21, 70],
+    //           anchorXUnits: 'pixels',
+    //           anchorYUnits: 'pixels',
+    //           opacity: 1,
+    //           src: this.assets_base + 'vehicle_color_7.png',
+    //           snapToPixel: false
+    //         })),
+    //         //Vehicle Label
+    //         text: new Text({
+    //           // text:
+    //           textAlign: 'center',
+    //           font: '11px Arial',
+    //           textBaseline: 'top',
+    //           fill: new Fill({ color: 'lightCyan' }),
+    //           stroke: new Stroke({ color: 'MediumBlue', width: 1 }),
+    //           offsetX: 0,
+    //           offsetY: 4,
+    //         })
+    //       }),
+    //     )
       
-      foundAddressFeature.setStyle(markerStyle);
-    }
+    //   foundAddressFeature.setStyle(markerStyle);
+    // }
 
     applyStyleToDistricts(feature){
       let plots_styles = [];
@@ -591,11 +591,32 @@ export class StyleService {
         }),
         new Style({
           image: new IconStyle(({
+            anchor: [21, 70],
+            anchorXUnits: 'pixels',
+            anchorYUnits: 'pixels',
+            opacity: 1,
+            src: this.assets_base + 'vehicle_color_3.png',
+            snapToPixel: false
+          })),
+          //Vehicle Label
+          text: new Text({
+            // text:
+            textAlign: 'center',
+            font: '11px Arial',
+            textBaseline: 'top',
+            fill: new Fill({ color: 'lightCyan' }),
+            stroke: new Stroke({ color: 'MediumBlue', width: 1 }),
+            offsetX: 0,
+            offsetY: 4,
+          })
+        }),
+        new Style({
+          image: new IconStyle(({
             anchor: [16, 70],
             anchorXUnits: 'pixels',
             anchorYUnits: 'pixels',
             opacity: 1,
-            src: 'assets/img/vehicle_color_3.png',
+            src: this.assets_base + 'iconmonstr-bicycle-1-32.png',
             snapToPixel: false
           }))
         })
