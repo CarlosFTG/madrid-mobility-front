8115156e4dbfe2976ccffc4e8f5b4f784ae3cad3
CarlosFTG <cftejedag@gmail.com>
2021-01-18 18:09:54 +0100

    setting error message in error dialog

--- src/app/components/errordialog/errordialog.component.ts
+++ src/app/components/errordialog/errordialog.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { DialogData } from '../nomadriddialog/nomadriddialog.component';
 
 @Component({
   selector: 'app-errordialog',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ErrordialogComponent implements OnInit {
 
-  constructor() { }
+  error: string
+
+  constructor(public dialogRef: MatDialogRef<ErrordialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) { }
 
   ngOnInit(): void {
+    this.setError();
+  }
+
+  setError(){
+    console.log(this.data)
+    //@ts-ignore
+    let mapquesturlSplitted = this.data.err.url.split('/')[2];
+    console.log(mapquesturlSplitted)
+    
+    //@ts-ignore
+    if(mapquesturlSplitted === 'www.mapquestapi.com'){
+      this.error = 'An error has occurred retrieving your location, please, try again in a few seconds'
+    }
+    
   }
 
 }
