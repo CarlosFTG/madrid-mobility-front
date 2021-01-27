0f95b973c110cc999edfec5ebc7468f8a1b2503d
CarlosFTG <cftejedag@gmail.com>
2021-01-27 17:40:19 +0100

    user service refactor

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -25,127 +25,132 @@ import { ErrordialogComponent } from '../components/errordialog/errordialog.comp
 export class UserService {
 
   userCity: string;
+  userPosition;
   format = new WKT();
   assets_base = 'assets/img/';
 
-  private userPositionOut = new BehaviorSubject<any[]>(null);
+  private userPositionOut = new BehaviorSubject<any>(null);
 
   userPosition$ = this.userPositionOut.asObservable();
 
-  //private REST_API_SERVER = "http://localhost:8081/api/auth/EMTServices/";
-  private REST_API_SERVER = "https://floating-reef-24535.herokuapp.com/api/auth/EMTServices/";
+  private REST_API_SERVER = "http://localhost:8081/api/auth/EMTServices/";
+  //private REST_API_SERVER = "https://floating-reef-24535.herokuapp.com/api/auth/EMTServices/";
 
   constructor(private httpClient: HttpClient, private mapService: MapService,
     private infoCardService: InfoCardService, public dialog: MatDialog, private styleService: StyleService) {
     this.getUserPosition();
   }
 
-  notifyUserPosition(userPosition) {
-    this.userPositionOut.next(userPosition)
+  // notifyUserPosition(userPosition) {
+  //   this.userPositionOut.next(userPosition)
+  // }
+
+  async getUserPosition() {
+    let coords = await this.fetchCoordinates();
+    //@ts-ignore
+    this.getGeoCoding(coords.coords)
+
   }
 
-  getUserPosition() {
-    let http = this.httpClient;
-    let userCity = this.userCity;
-    let mapService = this.mapService;
-    let format = this.format;
-    let assets_base = this.assets_base;
-    let notifyUserPosition = this.notifyUserPosition;
-    let dialog = this.dialog;
-    let styleService = this.styleService;
-    let REST_API_SERVER = this.REST_API_SERVER;
-
-    navigator.geolocation.getCurrentPosition(function (location) {
-      var lat = location.coords.latitude;
-      var lng = location.coords.longitude;
-
-
-      let locationObj = {
-        'lat:': lat,
-        'lng': lng
-      }
-      http.get('https://www.mapquestapi.com/geocoding/v1/reverse?key=rap9nA00BZ9zIZLP1eWHyyyrRkqGdFVX&location=' + location.coords.latitude + ',' + location.coords.longitude).subscribe(
-        res => {
-          let markerStyle = [];
-          //@ts-ignore
-          localStorage.setItem('userLocationAddress', res.results[0].locations[0].street)
-
-          //@ts-ignore
-          userCity=res.results[0].locations[0].adminArea5;
-
-          http.get(REST_API_SERVER+'registerVisit',{
-            params: {
-              'userCity': userCity,
-            }
-          }
-          ).subscribe(
-            res =>{
-            },
-            err=>{
-              
-            }
-          )
-
-          //@ts-ignore
-          if (res.results[0].locations[0].adminArea5 === 'Madrid') {
-            //@ts-ignore
-            let formatCoords = 'POINT(' + res.results[0].providedLocation.latLng.lng + ' ' + res.results[0].providedLocation.latLng.lat + " 216.7" + ')';
-            //@ts-ignore
-            let formatCoords2 = 'POINT(' + res.results[0].providedLocation.latLng.lng + ' ' + res.results[0].providedLocation.latLng.lat + ')';
-
-            mapService.updateMapViewCenter(formatCoords2);
-
-            let userPositionCoords = format.readFeature(formatCoords.replace(
-              /[\W]*\S+[\W]*$/, '') + ')', { dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857' }).getGeometry().getCoordinates();
-
-            let userPositionFeature = new Feature({
-              geometry: new Point(userPositionCoords)
-            });
-
-            //@ts-ignore
-            mapService.sendUserPositionToInfoCard(res.results[0].providedLocation.latLng);
-
-            styleService.applyStyleToUser(userPositionFeature);
-
-            let userPositionCollection = new Collection;
-
-            userPositionCollection.push(userPositionFeature);
-
-            let userPositionLayer = new VectorLayer({
-              name: 'userPosition',
-              source: new VectorSource({
-                features: userPositionCollection
-              })
-            });
-
-            mapService.map$.addLayer(userPositionLayer);
-            //@ts-ignore
-            userCity = res.results[0].locations[0].adminArea5;
-          } else {
-            const dialogRef = dialog.open(NomadriddialogComponent, {
-              width: '600px',
-            });
-
-            dialogRef.afterClosed().subscribe(result => {
-              console.log(result)
-            });
-
-          }
-
-        }, err =>{
-          const dialogRef = dialog.open(ErrordialogComponent, {
-            width: '600px',
-            data: {err:err}
-          });
-        }
-      )
-
-      return locationObj;
+  getCoordinates() {
+    return new Promise(function (resolve, reject) {
+      navigator.geolocation.getCurrentPosition(resolve, reject);
     });
   }
 
-  registerUserCity() {
-        
-      
+  async fetchCoordinates() {
+    // notice, no then(), cause await would block and 
+    // wait for the resolved result
+    const position = await this.getCoordinates();
+
+    // Actually return a value
+    return position;
   }
+
+  getGeoCoding(coords) {
+    let lat = coords.latitude;
+    let lng = coords.longitude;
+    this.httpClient.get('https://www.mapquestapi.com/geocoding/v1/reverse?key=rap9nA00BZ9zIZLP1eWHyyyrRkqGdFVX&location='
+      + lat + ',' + lng).subscribe(
+        res => {
+          //@ts-ignore
+          if(res.results[0].locations[0] != undefined){
+            //@ts-ignore
+            localStorage.setItem('userLocationAddress', res.results[0].locations[0].street);
+          }
+          
+          //@ts-ignore
+          let userCity = res.results[0].locations[0].adminArea5;
+          this.registerVisit(userCity);
+          //@ts-ignore
+          this.checkIfUserInMadrid(userCity,res.results[0].providedLocation.latLng.lng, res.results[0].providedLocation.latLng.lat, res.results[0].providedLocation.latLng);
+        },
+        err => {
+          const dialogRef = this.dialog.open(ErrordialogComponent, {
+            width: '600px',
+            data: { err: err }
+          });
+        }
+      )
+  }
+
+  registerVisit(userCity) {
+    this.httpClient.get(this.REST_API_SERVER + 'registerVisit', {
+      params: {
+        'userCity': userCity,
+      }
+    }
+    ).subscribe(
+      res => {
+      },
+      err => {
+
+      }
+    )
+  }
+
+  checkIfUserInMadrid(userCity, lng,lat,latLng){
+    if (userCity === 'Madrid') {
+      let formatCoords = 'POINT(' + lng + ' ' + lat + " 216.7" + ')';
+      let formatCoords2 = 'POINT(' + lng + ' ' + lat + ')';
+
+      this.mapService.updateMapViewCenter(formatCoords2);
+
+      let userPositionCoords = this.format.readFeature(formatCoords.replace(
+        /[\W]*\S+[\W]*$/, '') + ')', { dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857' }).getGeometry().getCoordinates();
+
+      let userPositionFeature = new Feature({
+        geometry: new Point(userPositionCoords)
+      });
+
+      //@ts-ignore
+      this.mapService.sendUserPositionToInfoCard(latLng);
+
+      this.styleService.applyStyleToUser(userPositionFeature);
+
+      let userPositionCollection = new Collection;
+
+      userPositionCollection.push(userPositionFeature);
+
+      let userPositionLayer = new VectorLayer({
+        name: 'userPosition',
+        source: new VectorSource({
+          features: userPositionCollection
+        })
+      });
+
+      this.mapService.map$.addLayer(userPositionLayer);
+      //@ts-ignore
+    } else {
+      const dialogRef = this.dialog.open(NomadriddialogComponent, {
+        width: '600px',
+      });
+
+      dialogRef.afterClosed().subscribe(result => {
+        console.log(result)
+      });
+
+    }
+  }
+
 }
