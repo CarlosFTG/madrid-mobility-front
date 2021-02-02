d5679e2b58da6acba26d9fc20efc30e59590ad02
CarlosFTG <cftejedag@gmail.com>
2021-02-02 15:44:06 +0100

    get user language

--- src/app/components/welcome/welcome.component.ts
+++ src/app/components/welcome/welcome.component.ts
@@ -8,11 +8,25 @@ import {Router} from '@angular/router'
 })
 export class WelcomeComponent implements OnInit {
 
+  languageEN: boolean;
+
   constructor(private router: Router) { }
 
   ngOnInit(): void {
+    this.getLanguage();
   }
 navigateToMadridMobility(){
   this.router.navigate(['map']);
+}
+
+getLanguage(){
+  let languageMavigator =  window.navigator.language;
+  
+  if(languageMavigator === 'es-ES'){
+    this.languageEN = false;
+  }else{
+    this.languageEN = true;
+  }
+
 }
 }
